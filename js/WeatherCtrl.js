15b65572acbc353b864adc27dc6ce4d35280cd78
GiornoS <sven.giorno-dit-journo@student.eEcp.fr>
2015-01-27 18:11:25 +0100

    dgd

--- js/WeatherCtrl.js
+++ js/WeatherCtrl.js
@@ -113,8 +113,7 @@ angular.module('app', ['ionic'])
 	}
 	
 	$scope.Math = Math;		//Importation du module Math pour arrondir les températures
-	//$scope.geolocate();		// On initialise la fonction de géolocalisation au lancement de l'application
-	$scope.loader_var=true;
+	$scope.geolocate();		// On initialise la fonction de géolocalisation au lancement de l'application
 });
 
 
