3df2ecac533c8c0c807358f3742af745f10694f2
GiornoS <sven.giorno-dit-journo@student.eEcp.fr>
2015-01-28 22:14:32 +0100

    Debug du probème de géolocalisation

--- js/WeatherCtrl.js
+++ js/WeatherCtrl.js
@@ -91,7 +91,6 @@ angular.module('app', ['ionic'])
 
 	//~ On récupère les coordonnées dans une variable à part pour pouvoir afficher l'adresse du lieu où l'on est
 	httpSuccessGeolocate = function(response){
-		$ionicLoading.hide();
 		navigator.geolocation.getCurrentPosition(function(position){
 		$http.get("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude +"," + position.coords.longitude + "&sensor=false").success(httpSuccessGeolocateSuccess).error(httpError)
 		})
